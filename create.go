b4830c30b80390c9ef9afb15d91c548a06ad5b83
crazycs <crazycs520@gmail.com>
2018-09-08 00:02:45 +0800

    add transaction test code and select code

--- create.go
+++ create.go
@@ -1,18 +1,30 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"os"
 
-	"github.com/pingcap/schrodinger-test/util"
+	_ "github.com/go-sql-driver/mysql" // mysql driver
 )
 
-func main() {
+func getCli() *sql.DB {
 	dbAddr := "127.0.0.1:4000"
 	dbDSN := fmt.Sprintf("root:@tcp(%s)/%s", dbAddr, "test")
-	db, err := util.OpenDB(dbDSN, 8)
+	db, err := sql.Open("mysql", dbDSN)
 	if err != nil {
-		return
+		os.Exit(0)
 	}
+	db.SetMaxOpenConns(20)
+	return db
+}
+
+func main() {
+	transaction()
+}
+
+func create() {
+	db := getCli()
 
 	numRow := 100
 	startNum := 0
@@ -52,4 +64,88 @@ func main() {
 		}
 	}
 
+}
+
+func transaction() {
+	db1 := getCli()
+	db2 := getCli()
+	db3 := getCli()
+	sql := `drop table if exists t;
+	CREATE TABLE t (
+  a int(11) NOT NULL AUTO_INCREMENT,
+  b int(11) DEFAULT NULL,
+  PRIMARY KEY (a)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+insert into t values(1, 0);`
+
+	db3.Exec(sql)
+	fmt.Println("initial")
+	selectAndPrint(db3, "select b from t where a = 1")
+	for i := 0; i < 10; i++ {
+		db1.Exec("SET autocommit=0")
+		db2.Exec("update t set b=2 where a=1")
+		db1.Exec("begin")
+		selectAndPrint(db1, "select b from t where a = 1")
+		db1.Exec("update t set b=0 where a=1")
+		db1.Exec("commit")
+	}
+}
+
+func selectAndPrint(db *sql.DB, sql string) {
+	// execute
+	rows, err := db.Query(sql)
+
+	if err == nil {
+		defer rows.Close()
+	}
+	// When column is removed, SELECT statement may return error so that we ignore them here.
+
+	if err != nil {
+		return
+	}
+
+	// Read all rows.
+	var actualRows [][]interface{}
+	for rows.Next() {
+		cols, err1 := rows.Columns()
+		if err1 != nil {
+			return
+		}
+
+		// See https://stackoverflow.com/questions/14477941/read-select-columns-into-string-in-go
+		rawResult := make([][]byte, len(cols))
+		result := make([]interface{}, len(cols))
+		dest := make([]interface{}, len(cols))
+		for i := range rawResult {
+			dest[i] = &rawResult[i]
+		}
+
+		err1 = rows.Scan(dest...)
+		if err1 != nil {
+			return
+		}
+
+		for i, raw := range rawResult {
+			if raw == nil {
+				result[i] = "NULL"
+			} else {
+				val := string(raw)
+
+				result[i] = val
+			}
+		}
+
+		actualRows = append(actualRows, result)
+	}
+	if rows.Err() != nil {
+		return
+	}
+
+	for _, row := range actualRows {
+		rowString := ""
+		for _, col := range row {
+			rowString += fmt.Sprintf("%v,", col)
+		}
+		fmt.Println(rowString)
+	}
 }
